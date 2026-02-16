4c407bf96e08032a9fe6832404e37bdb9f9b0f0e
Noel <acrecapv@gmail.com>
2026-02-16 21:39:36 +0800

    chore: configure Firebase emulators and connect in dev mode

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,11 @@
 import firestore from '@react-native-firebase/firestore'
 import auth from '@react-native-firebase/auth'
 
+if (__DEV__) {
+  const EMULATOR_HOST = 'localhost'
+  firestore().useEmulator(EMULATOR_HOST, 8080)
+  auth().useEmulator(`http://${EMULATOR_HOST}:9099`)
+}
+
 export const db = firestore()
 export const firebaseAuth = auth()
